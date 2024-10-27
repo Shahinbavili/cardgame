16b43a8aa8e9d8da1c198406c0b493775c332f7c
shahin <shahin.aboulhassanibavili2021@campus-eni.fr>
2024-10-27 11:16:39 +0100

    initial commit

---
+++ src/main/java/com/shahin/cardgame/model/Suit.java
@@ -0,0 +1,18 @@
+package com.shahin.cardgame.model;
+
+public enum Suit {
+    DIAMONDS(1),
+    HEARTS(2),
+    SPADES(3),
+    CLUBS(4);
+
+    private final int suit;
+
+    Suit(int value) {
+        this.suit = value;
+    }
+
+    public int value() {
+        return suit;
+    }
+}
